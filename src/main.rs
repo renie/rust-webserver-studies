0c8b12326434548b8264c2395e0ad79b6b94dc0e
Renie Siqueira <renie.webdev@gmail.com>
2022-03-14 17:58:26 +0200

    chore: refactoring.

--- src/main.rs
+++ src/main.rs
@@ -19,31 +19,24 @@ fn handle_connection(mut stream: TcpStream) {
 
     let get = b"GET / HTTP/1.1\r\n";
 
-    if buffer.starts_with(get) {
-        let contents = fs::read_to_string("response.html").unwrap();
+    let (status_line, file) = 
+        if buffer.starts_with(get) {
+            ("HTTP/1.1 200 OK", "response.html")
+        } else {
+            ("HTTP/1.1 404 NOT FOUND", "404.html")
+        };
 
-        let response = format!(
-            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-            contents.len(),
-            contents
-        );
-
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-    } else {
-        let contents = fs::read_to_string("404.html").unwrap();
-
-        let response = format!(
-            "HTTP/1.1 404 NOT FOUND\r\nContent-Length: {}\r\n\r\n{}",
-            contents.len(),
-            contents
-        );
-
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-    }
-
+    let contents = fs::read_to_string(file).unwrap();
     
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 
     println!("Request received!");
 }
